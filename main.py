7b0437528d08ad3a939b3242a54f00277caba081
bugbiteme <leon.s.levy@gmail.com>
2025-01-22 14:14:14 -0800

    cicd

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ service_remote = os.getenv('SERVICE_REMOTE', 'python-rest-api-b')
 
 @app.get("/")
 async def usage():
-    return {"message": "See /docs for API usage - test"}
+    return {"message": "See /docs for API usage"}
 
 @app.get("/hello")
 async def read_hello():
